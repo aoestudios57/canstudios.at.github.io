566531396f9b2912a0ff3bc07736e11176e150f1
Atakan Özkan <107040470+aoestudios57@users.noreply.github.com>
2024-01-09 01:26:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+
+// Ändere den Text des Paragraphs
+document.querySelector('.text p').innerText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus rem eos aliquid quo rerum temporibus ipsum distinctio numquam ut omnis placeat, nam sint atque quos dolorem laborum? Rerum, esse dolorem'
+;
+// Ändere den Titel der Seite
+document.title = 'Dynamische Seite';
+
+// Füge einen neuen HTML-Code hinzu
+const newCode = 
+document.querySelector('.code').innerHTML += newCode;
+
+// Dynamische Klassen hinzufügen
+document.querySelector('.od').classList.add('neueKlasse');
